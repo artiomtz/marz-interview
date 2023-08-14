20858bc0d11b7d64abf75b27d45bc10a926c3027
Artiom Tsimkin <artiomtz@gmail.com>
2023-08-13 22:10:39 -0400

    added products page tests

---
+++ webapp/src/pages/ProductsPage/ProductsPage.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { PRODUCTS_URL } from "../ApiHelper";
+import { render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import ProductsPage from "./ProductsPage";
+
+describe("ProductsPage", () => {
+  it("shouldDisplayLoadingSpinner", () => {
+    render(
+      <MemoryRouter>
+        <ProductsPage />
+      </MemoryRouter>
+    );
+    expect(screen.getByTestId(`loading-spinner-container`)).toBeInTheDocument();
+  });
+  it("shouldDisplayProductsContainer", async () => {
+    // set up mock for axios.get
+    const response = {
+      data: [
+        {
+          ProductID: 1,
+          ProductName: "Hat",
+          ProductPhotoURL:
+            "https://cdn.pixabay.com/photo/2014/08/25/09/26/monster-426995_1280.jpg",
+          ProductStatus: "Active",
+        },
+      ],
+      message: "",
+    };
+    const server = setupServer(
+      rest.get(PRODUCTS_URL, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(response));
+      })
+    );
+    server.listen();
+    render(
+      <MemoryRouter>
+        <ProductsPage />
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      expect(screen.getByTestId(`products-container`)).toBeInTheDocument();
+    });
+    server.close();
+  });
+  it("shouldDisplayErrorMessage", async () => {
+    // set up mock for axios.get
+    const response = {
+      data: [],
+      message: "Error",
+    };
+    const server = setupServer(
+      rest.get(PRODUCTS_URL, (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json(response));
+      })
+    );
+    server.listen();
+    render(
+      <MemoryRouter>
+        <ProductsPage />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId(`error-container`)).toBeInTheDocument();
+    });
+    server.close();
+  });
+});
